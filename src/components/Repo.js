10c2c261659988463840c7f25c029308095f6eec
Daniel Trandafir <danieltrandafir@live.co.uk>
2023-03-13 00:26:37 +0000

    style repo card

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,5 +1,26 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Stack from 'react-bootstrap/Stack';
 
 
-export const Repo = () => {
-    return <div>Repos</div>
+export const Repo = ({repo}) => {
+    return (
+        <Card className="repo-card m-3" key={repo.id}>
+            <Card.Img variant="top" src={repo?.owner?.avatar_url} />
+            <Card.Body>
+              <Card.Title>{repo.name}</Card.Title>
+              {repo.description && <Card.Text>{repo.description}</Card.Text>}
+            </Card.Body>
+            <Card.Footer className="text-center">
+              <Stack direction="horizontal" className="justify-content-between">
+                <Button variant="light" href={repo.html_url} target="_blank">
+                    <i class="fa-brands fa-github"></i>
+                </Button>
+                <Button variant="light" href={repo.html_url} target="_blank">
+                    <i class="fa-solid fa-trash"></i>   
+                </Button>
+              </Stack>
+            </Card.Footer>
+          </Card>
+    )
 }
