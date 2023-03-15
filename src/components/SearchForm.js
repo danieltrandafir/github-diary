8506fccecfc197cfd9cb1cd1b9b67e27af21aded
Daniel Trandafir <danieltrandafir@live.co.uk>
2023-03-15 16:53:33 +0000

    add search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,27 @@
 
 
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 export const SearchForm = () => {
-    return <div>SearchForm</div>
+
+    return (
+        <Form>
+            <Form.Group className="mb-3">
+                <Form.Control
+                type="text" placeholder="Enter GitHub username"                
+                />
+               
+                <Form.Text className="text-danger">
+                    Please enter a valid GitHub Username.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3 text-center">
+                <Button variant="success" type="submit">
+                Search
+                </Button>
+            </Form.Group>
+        </Form>
+    )
 }
