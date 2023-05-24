b5d00055ce692c58f96b1115eec91d3e2571bd7e
Daniel Trandafir <danieltrandafir@live.co.uk>
2023-05-24 17:02:37 +0100

    redo SearchForm's state

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,45 +1,22 @@
-
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
-export const SearchForm = ({handleApiTrigger}) => {
-    const [userName, setUserName] = useState("")
-    const [error, setError] = useState(false)
+export const SearchForm = ({ handleApiTrigger }) => {
+  return (
+    <Form>
+      <Form.Group className="mb-3">
+        <Form.Control type="text" placeholder="Enter GitHub username" />
+        <Form.Text className="text-danger">
+          Please enter a valid GitHub Username.
+        </Form.Text>
+      </Form.Group>
 
-    const handleOnChange = ({currentTarget}) => {
-        setUserName(currentTarget.value)
-    }
-
-    // get event object
-    const handleSubmit = (event) => {
-        // prevent default
-        event.preventDefault()
-
-        if(!userName){
-            setError(true)
-        } else {setError(false)}
-        // trigger API request
-        handleApiTrigger(userName)
-    };
-
-    return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3">
-                <Form.Control
-                type="text" placeholder="Enter GitHub username" value={userName} onChange={handleOnChange}                
-                />
-               
-                {error && (<Form.Text className="text-danger">
-                    Please enter a valid GitHub Username.
-                </Form.Text>)}
-            </Form.Group>
-
-            <Form.Group className="mb-3 text-center">
-                <Button variant="success" type="submit">
-                Search
-                </Button>
-            </Form.Group>
-        </Form>
-    )
-}
+      <Form.Group className="mb-3 text-center">
+        <Button variant="success" type="submit">
+          Search
+        </Button>
+      </Form.Group>
+    </Form>
+  );
+};
