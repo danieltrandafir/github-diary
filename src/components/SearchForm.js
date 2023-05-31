6b13c05db6151b0e799c2dae9451e5453fbeffcb
Daniel Trandafir <danieltrandafir@live.co.uk>
2023-05-31 15:42:14 +0100

    track state of username

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,10 +3,21 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 export const SearchForm = ({ handleApiTrigger }) => {
+  const [userName, setUsername] = useState();
+
+  const handleOnChange = (currentTarget) => {
+    setUsername(currentTarget.value);
+  };
+
   return (
     <Form>
       <Form.Group className="mb-3">
-        <Form.Control type="text" placeholder="Enter GitHub username" />
+        <Form.Control
+          type="text"
+          placeholder="Enter GitHub username"
+          value={userName}
+          onChange={handleOnChange}
+        />
         <Form.Text className="text-danger">
           Please enter a valid GitHub Username.
         </Form.Text>
